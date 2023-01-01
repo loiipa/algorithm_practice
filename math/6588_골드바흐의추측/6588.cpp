4153833aec532712ca4cbeb1cc2ba525f2525841
loiipa <devcjang@gmail.com>
2023-01-01 18:43:14 +0900

    Fix 골드바흐의 추측 for space
    tab -> space (테스트)

--- math/6588_골드바흐의추측/6588.cpp
+++ math/6588_골드바흐의추측/6588.cpp
@@ -4,75 +4,75 @@
 
 namespace
 {
-	constexpr int NUMBER = 1000000;
+    constexpr int NUMBER = 1000000;
 }
 
 class PrimeNumber
 {
 public:
-	PrimeNumber(PrimeNumber& primeNumber) = delete;
-	void operator=(const PrimeNumber&) = delete;
+    PrimeNumber(PrimeNumber& primeNumber) = delete;
+    void operator=(const PrimeNumber&) = delete;
 
-	static std::shared_ptr<PrimeNumber> GetInstance();
-	const std::array<bool, NUMBER + 1>& GetPrimeArray() const { return primeArray; }
+    static std::shared_ptr<PrimeNumber> GetInstance();
+    const std::array<bool, NUMBER + 1>& GetPrimeArray() const { return primeArray; }
 
-	int GetGoldbachNumber(const int& num) const;
+    int GetGoldbachNumber(const int& num) const;
 
 protected:
-	PrimeNumber() { SetPrime(NUMBER); }
-	void SetPrime(const int& num);
+    PrimeNumber() { SetPrime(NUMBER); }
+    void SetPrime(const int& num);
 
-	static std::shared_ptr<PrimeNumber> primeNumber;
-	std::array<bool, NUMBER + 1> primeArray{};
+    static std::shared_ptr<PrimeNumber> primeNumber;
+    std::array<bool, NUMBER + 1> primeArray{};
 };
 
 std::shared_ptr<PrimeNumber> PrimeNumber::GetInstance()
 {
-	if (primeNumber == nullptr)
-		primeNumber = std::shared_ptr<PrimeNumber>(new PrimeNumber);
-	return primeNumber;
+    if (primeNumber == nullptr)
+        primeNumber = std::shared_ptr<PrimeNumber>(new PrimeNumber);
+    return primeNumber;
 }
 
 void PrimeNumber::SetPrime(const int& num)
 {
-	for (int prime = 2; prime * prime < num; prime++)
-	{
-		if (!primeArray[prime])
-		{
-			for (int i = prime; i <= num / prime; i++)
-				primeArray[prime * i] = true;
-		}
-	}
+    for (int prime = 2; prime * prime < num; prime++)
+    {
+        if (!primeArray[prime])
+        {
+            for (int i = prime; i <= num / prime; i++)
+                primeArray[prime * i] = true;
+        }
+    }
 }
 
 int PrimeNumber::GetGoldbachNumber(const int& num) const
 {
-	int numFirst = 0;
-	for (auto i = 3; i <= num / 2; i += 2)
-	{
-		if (primeArray[i] == false && primeArray[num - i] == false)
-			return i;
-	}
-	return numFirst;
+    int numFirst = 0;
+    for (auto i = 3; i <= num / 2; i += 2)
+    {
+        if (primeArray[i] == false && primeArray[num - i] == false)
+            return i;
+    }
+    return numFirst;
 }
 
 std::shared_ptr<PrimeNumber> PrimeNumber::primeNumber = nullptr;
 
 int main()
 {
-	auto primeNumber = PrimeNumber::GetInstance();
-	int num = 1;
+    auto primeNumber = PrimeNumber::GetInstance();
+    int num = 1;
 
-	while (num > 0)
-	{
-		scanf("%d", &num);
-		if (num / 2 == 0 || num > NUMBER)
-			continue;
+    while (num > 0)
+    {
+        scanf("%d", &num);
+        if (num / 2 == 0 || num > NUMBER)
+            continue;
 
-		auto numFirst = primeNumber->GetGoldbachNumber(num);
-		if (num > 1 && numFirst > 0)
-			printf("%d = %d + %d \n", num, numFirst, num - numFirst);
-		else
-			printf("Goldbach's conjecture is wrong.\n");
-	}
+        auto numFirst = primeNumber->GetGoldbachNumber(num);
+        if (num > 1 && numFirst > 0)
+            printf("%d = %d + %d \n", num, numFirst, num - numFirst);
+        else
+            printf("Goldbach's conjecture is wrong.\n");
+    }
 }
